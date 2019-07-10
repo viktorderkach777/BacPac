cba8d5c218e9c7d371dfa2f671c37c7b4d465ff7
viktorderkach777 <viktorderkach777@yandex.ua>
2019-07-10 20:27:05 +0300

    first commit

---
+++ TestBackup/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.SqlServer.Dac;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestBackup
+{
+    class Program
+    {
+        private static Timer timer;
+
+        static void Main(string[] args)
+        {            
+
+            var timerState = new TimerState { Counter = 0 };
+
+            timer = new Timer(
+                callback: new TimerCallback(TimerTask),
+                state: timerState,
+                dueTime: 1000,
+                period: 60000);
+
+            while (timerState.Counter <= 10)
+            {
+                Task.Delay(60000).Wait();
+               
+            }
+
+            timer.Dispose();
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff}: done.");          
+        }
+
+        private static void TimerTask(object timerState)
+        {
+            DateTime now = DateTime.Now;
+
+            var minute = now.Minute;
+
+            if(minute == 30 || minute == 0 )
+            {
+                string data = now.ToString("dd-MM-yyyy-HH-mm-ss");
+                Console.WriteLine(data);
+
+                var connectionString = "Server=DESKTOP-08EQ49B; " +
+                    "Database=bober; User Id=test; Password=123456; " +
+                    "MultipleActiveResultSets=true";
+                var services = new DacServices(connectionString);
+                services.Message += (sender, e) =>
+                {
+                    // If you use a lock file,
+                    // this would be a good location to extend the lease
+                };
+
+
+                services.ExportBacpac("D://" + data + ".bacpac", "bober");
+                var state = timerState as TimerState;
+                Interlocked.Increment(ref state.Counter);
+            }
+           
+        }
+
+        class TimerState
+        {
+            public int Counter;
+        }
+    }
+}
